ada5e266039cdeb982f54bcb867da4bfd402ef8b
Timothy Logan <timothy.logan@gmail.com>
2016-07-10 22:45:07 -0500

    Added skeleton iOS project

---
+++ src/ios/babelpet/babelpet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babelpet
+//
+//  Created by Timothy Logan on 7/10/16.
+//  Copyright © 2016 Shintako LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
